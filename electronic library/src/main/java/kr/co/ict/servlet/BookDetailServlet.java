e6c849e816df73b285e5741eeab6bbd169c2a448
jjjjjjji-hun <lingcke@naver.com>
2022-03-08 16:21:28 +0900

    김지훈 : book_search / search_check / book_detail 로직 완성 및 BookDAO 추가 수정
    북 서치 로직 완성

--- electronic library/src/main/java/kr/co/ict/servlet/BookDetailServlet.java
+++ electronic library/src/main/java/kr/co/ict/servlet/BookDetailServlet.java
@@ -1,11 +1,16 @@
 package kr.co.ict.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kr.co.ict.BookDAO;
+import kr.co.ict.BookVO;
 
 /**
  * Servlet implementation class BookDetailServlet
@@ -26,8 +31,23 @@ public class BookDetailServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getParameter(")
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// request.getParameter 를 이용해 도서명 가져오기
+		String BName = request.getParameter("bName");
+		// 디버깅
+		System.out.println(BName);
+		// 다오 생성
+		BookDAO dao = BookDAO.getInstance();
+		// 한글
+		request.setCharacterEncoding("utf-8");
+		// 다오에서 해당 도서명에 대한 정보 가져오고 디버깅
+		BookVO Book = dao.getBookData(BName);
+		System.out.println(Book);
+		// 바인딩
+		request.setAttribute("bName", Book);
+		// 포워딩
+		RequestDispatcher dp = request.getRequestDispatcher("/book/book_detail.jsp");
+		dp.forward(request, response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 }
--- electronic library/src/main/webapp/book/search_check.jsp
+++ electronic library/src/main/webapp/book/search_check.jsp
@@ -7,13 +7,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
 	<!-- 검색한 도서 정보 -->
 	<h1>검색된 도서명</h1>
-	<table border="1">
+	<table class="table table-hover">
 		
 			<thead>
 				<th>도서 번호</th>
@@ -24,11 +25,13 @@
 			</thead>
 			<tbody>
 			<!-- 사용자가 검색한 단어를 포함하는 책 리스트 보여주기 코드 찾는중 -->
+				<c:forEach var="SearchList" items="${BookList}">
 					<tr>
-						<td>${allBookList.bNum}</td>
-						<td><a href="./book/book_detail.jsp">${allBookList.bName}</a></td>
-						<td>${allBookList.bCategory}</td>
+						<td>${SearchList.bNum}</td>
+						<td><a href="http://localhost:8181/electronic_library/BookDetailServlet?bName=${SearchList.bName }">${SearchList.bName }</a></td>
+						<td>${SearchList.bCategory}</td>
 					</tr>
+				</c:forEach>
 			</tbody>
 		
 			</table>
