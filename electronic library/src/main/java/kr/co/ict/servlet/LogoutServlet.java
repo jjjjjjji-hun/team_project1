6924fe8974abe67f117dff05c8406d853376aae6
iwalkbyfaith <godownthestreet@naver.com>
2022-03-15 12:07:24 +0900

    고다은 : 로그아웃 서블릿 수정 (doGet으로 들어왔을때 doPost로 보내기)
    버튼을 눌렀을 때 post 방식으로 보냈는데 들어오는건 doGet으로 들어와서 그냥 그 밑에 메서드 하나 추가해서 doPost로 보냈음

--- electronic library/src/main/java/kr/co/ict/servlet/LogoutServlet.java
+++ electronic library/src/main/java/kr/co/ict/servlet/LogoutServlet.java
@@ -27,20 +27,20 @@ public class LogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet()으로 들어옴");
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost()로 들어옴");
 		
-     HttpSession session = request.getSession();
-		
-	 session.invalidate();
-     
-     response.sendRedirect("http://localhost:8181/electronic_library/main_Page.jsp");
+	     HttpSession session = request.getSession();
+		 session.invalidate();
+	     
+	     response.sendRedirect("http://localhost:8181/electronic_library/main_Page.jsp");
 	}
 
 }
